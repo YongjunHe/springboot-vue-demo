f45b2d09ea0c403e2fec02bb9cbe7b808a6f34fc
Nju141250047 <1169357116@qq.com>
2018-03-18 22:33:38 +0800

    initial vuejs

---
+++ FrontVue/src/page/home/home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <router-link to="/home/login">Login</router-link>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "home"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ FrontVue/src/page/login/login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <h1>Login</h1>
+      <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "login",
+    data(){
+      return{
+        msg: 'enter your message'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
